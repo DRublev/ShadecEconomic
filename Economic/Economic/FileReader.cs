ea8ea0885311d534969cde256f9a10b7d0c13e2f
DRublev <aringai09>
2018-08-25 23:47:41 +0300

    Build dll

---
+++ Economic/Economic/FileReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace Economic
+{
+	public static class FileReader
+	{
+		public static string ReadFile(string filepath)
+		{
+			string content = null;
+
+			/*new Thread(() =>
+			{
+				
+			}).Start();*/
+			if (!File.Exists(filepath))
+			{
+				content = "File not found";
+			}
+			else
+			{
+				content = File.ReadAllText(filepath);
+			}
+
+			return content;
+		}
+	}
+}
