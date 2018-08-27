c5d7862018c583c578116244b22db592f274c418
DRublev <aringai09>
2018-08-27 12:20:44 +0300

    Fixed dependency error

---
+++ Economic/Economic/Entry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Maca134.Arma.DllExport;
+
+namespace Economic
+{
+    public class Entry
+    {
+		[ArmaDllExport]
+		public static string Invoke(string input, int size)
+		{
+			return "Works: " + input;
+		}
+    }
+}
